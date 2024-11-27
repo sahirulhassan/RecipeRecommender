527e88bad83c77845c88713abc8cc4c7bf452cf7
Sahir Ul Hassan <sahirulhasan2002@gmail.com>
2024-11-27 15:16:38 +0000

    Included a surpriseMe method that returns a random recipe.

--- src/main/java/Search.java
+++ src/main/java/Search.java
@@ -1,6 +1,9 @@
+import tech.tablesaw.api.Row;
 import tech.tablesaw.api.Table;
+import utility.Input;
 
 import java.util.Arrays;
+import java.util.Random;
 
 public class Search {
 
@@ -10,21 +13,35 @@ public class Search {
         this.dataset = dataset;
     }
 
-    public Table byIngredients(String[] query) {
+    public Table byIngredients() {
+        String[] query = Input.userStringArr();
         return dataset
                 .where(dataset
                         .textColumn("ingredients")
-                        .eval(entry -> {
-                            return Arrays.stream(query)
+                        .eval(entry -> Arrays.stream(query)
                                     .allMatch(queryIngredient -> entry.toLowerCase()
-                                            .contains(queryIngredient.toLowerCase()));
-                        }));
+                                            .contains(queryIngredient.toLowerCase()))));
     }
 
-        public Table byName(String name) {
+    public Table byName() {
+        String name = Input.stringInput("Search recipe:");
         return dataset
                 .where(dataset
                         .textColumn("name")
                         .eval(entry -> entry.toLowerCase().contains(name.toLowerCase())));
+    }
+
+    public Table byKeywords() {
+        String[] keywords = Input.userStringArr();
+        return dataset
+                .where(dataset.textColumn("tags")
+                        .eval(entry -> Arrays.stream(keywords)
+                                .allMatch(keyword -> entry.toLowerCase()
+                                        .contains(keyword.toLowerCase()))));
+    }
+
+    public Row surpriseMe() {
+        Random random = new Random();
+        return dataset.row(random.nextInt(dataset.rowCount()));
     }
 }
