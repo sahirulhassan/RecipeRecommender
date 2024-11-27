9a69528753e03e36c2863d50a5ed754623d3f427
Sahir Ul Hassan <sahirulhasan2002@gmail.com>
2024-11-27 18:23:54 +0000

    Streamlined the input class.

--- src/main/java/utility/Input.java
+++ src/main/java/utility/Input.java
@@ -3,46 +3,22 @@ package utility;
 import java.util.Scanner;
 
 public class Input {
-    public static String stringInput(String msg, Scanner scanner) {
-        System.out.print(msg);
+
+    private static final Scanner scanner = new Scanner(System.in); //
+
+    public static String stringInput(String msg) {
+        System.out.println(msg);
         return scanner.nextLine();
     }
 
-    public static String stringInput(String msg) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print(msg);
-        String input = scanner.nextLine();
-        scanner.close();
-        return input;
-    }
-
-    public static int intInput(String msg, Scanner scanner) {
-        System.out.print(msg);
-        return scanner.nextInt();
-    }
-
     public static int intInput(String msg) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print(msg);
-        int input = scanner.nextInt();
-        scanner.close();
-        return input;
-    }
-
-    public static int[] userArr() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the number of integers in your array: ");
-        int size = scanner.nextInt();
-        int[] arr = new int[size];
-        System.out.println("Enter the elements of your array; ");
-        for (int i = 0; i < size; i++) {
-            arr[i] = scanner.nextInt();
-        }
-        return arr;
+        System.out.println(msg);
+        int output = scanner.nextInt();
+        scanner.nextLine();
+        return output;
     }
 
     public static String[] userStringArr() {
-        Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the number of Strings in your array: ");
         int size = scanner.nextInt();
         scanner.nextLine(); // Consume the leftover newline
@@ -54,23 +30,10 @@ public class Input {
         return arr;
     }
 
-    public static int[][] user2dArr() {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Enter the total number of rows: ");
-        int rows = scanner.nextInt();
-        System.out.println("Enter the total number of columns: ");
-        int cols = scanner.nextInt();
-
-        int[][] arr = new int[rows][cols];
-
-        System.out.println("Enter the elements of the array:");
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                System.out.printf("Element at " + i + ", " + j + ": ");
-                arr[i][j] = scanner.nextInt();
-            }
-        }
-        return arr;
+    public static boolean yesOrNo(String msg) {
+        System.out.println(msg);
+        boolean output = scanner.nextBoolean();
+        scanner.nextLine();
+        return output;
     }
 }
