3b32fdf6d13408f832152036f4e1d97938b1accd
Sahir Ul Hassan <sahirulhasan2002@gmail.com>
2024-11-27 18:24:20 +0000

    Moved logic to other relevant classes.

--- src/main/java/RecipeRecommender.java
+++ src/main/java/RecipeRecommender.java
@@ -1,28 +1,5 @@
-import tech.tablesaw.io.csv.CsvReadOptions;
-import tech.tablesaw.api.Table;
-import java.util.Scanner;
-import static utility.Input.userStringArr;
-
-
 public class RecipeRecommender {
-    public static Table datasetReader(String filepath) {
-        CsvReadOptions options = CsvReadOptions.builder(filepath)
-                .maxCharsPerColumn(13000)
-                .build();
-        return Table.read().csv(options);
-    }
-
     public static void main(String[] args) {
-        Table dataset = datasetReader("src/main/resources/dataset_cleaned.csv");
-        Search search = new Search(dataset);
-        String[] userQuery = userStringArr();
-        Table filteredDataset = search.byIngredients(userQuery);
-        //Table filteredDataset = search.byName("pizza");
-        View.recipesList(filteredDataset);
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the recipe no.:");
-        int selection = scanner.nextInt();
-        scanner.close();
-        View.fullRecipe(filteredDataset.row(selection));
+        Menu.mainMenu();
     }
 }
