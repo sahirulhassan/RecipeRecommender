a3582a949d35558d526b61014888a6055bdf8b78
Sahir Ul Hassan <sahirulhasan2002@gmail.com>
2024-11-27 19:22:24 +0000

    Updated the menu with a more robust logic.

--- src/main/java/Menu.java
+++ src/main/java/Menu.java
@@ -16,9 +16,9 @@ public class Menu {
         return Table.read().csv(options);
     }
 
-    public static void mainMenu() {
+
+    public static void mainMenu() throws Exception {
         masterLoop = true;
-        loop = true;
         System.out.println(View.centerAlign("Recipe Recommender", 100));
         while (masterLoop) {
             System.out.printf(
@@ -26,17 +26,14 @@ public class Menu {
                             "1. Search Recipes\n2. Surprise me\n3. Exit\n"
             );
             selection = Input.intInput("Select from the menu:");
+            loop = true;
             switch (selection) {
                 case 1:
                     searchMenu();
                     break;
                 case 2:
-                    while (loop) {
-                        Row surprise = search.surpriseMe();
-                        View.fullRecipe(surprise);
-                        loop = Input.yesOrNo("Do you want another random recipe?");
-                    }
-                    System.out.println("Going back to the main menu...");
+                    Row surprise = search.surpriseMe();
+                    View.fullRecipe(surprise);
                     break;
                 case 3:
                     System.out.println(View.centerAlign("Goodbye! Exiting...", 100));
@@ -46,35 +43,60 @@ public class Menu {
         }
     }
 
-    private static void searchMenu() {
-        masterLoop = true;
-        loop = true;
+    private static void searchSelector(int searchMethod) throws Exception {
         Table filteredDataset;
+        switch (searchMethod) {
+            case 1:
+                filteredDataset = search.byIngredients();
+                break;
+            case 2:
+                filteredDataset = search.byName();
+                break;
+            case 3:
+                filteredDataset = search.byKeywords();
+                break;
+            default:
+                throw new Exception();
+        }
         int recipeNo;
+        while (loop) {
+            System.out.println(View.centerAlign("Found Results", 100));
+            View.recipesList(filteredDataset);
+            recipeNo = Input.intInput("Enter -1 to go back or select the recipe no. to view it.");
+            if (recipeNo == -1) {
+                break;
+            }
+            View.fullRecipe(filteredDataset.row(recipeNo));
+            selection = Input.intInput("1. Back to the list\n2. Back to the search menu");
+            switch (selection) {
+                case 1:
+                    System.out.println("Going back to the recipes list...");
+                    break;
+                case 2:
+                    System.out.println("Going back to the Search Menu...");
+                    loop = false;
+                    break;
+            }
+        }
+    }
+
+    private static void searchMenu() throws Exception {
+        masterLoop = true;
         while (masterLoop) {
             System.out.print("1. Search by Ingredients\n2. Search by Recipe Names\n3. Search by Keywords\n4. Back\n");
             selection = Input.intInput("Select from the menu:");
+            Table filteredDataset;
+            int recipeNo;
+            loop = true;
             switch (selection) {
                 case 1:
-                    filteredDataset = search.byIngredients();
-                    View.recipesList(filteredDataset);
-                    recipeNo = Input.intInput("Enter -1 to go back or select the recipe no. to view it.");
-                    if (recipeNo == -1) {break;};
-                    View.fullRecipe(filteredDataset.row(recipeNo));
+                    searchSelector(1);
                     break;
                 case 2:
-                    filteredDataset = search.byName();
-                    View.recipesList(filteredDataset);
-                    recipeNo = Input.intInput("Enter -1 to go back or select the recipe no. to view it.");
-                    if (recipeNo == -1) {break;};
-                    View.fullRecipe(filteredDataset.row(recipeNo));
+                    searchSelector(2);
                     break;
                 case 3:
-                    filteredDataset = search.byKeywords();
-                    View.recipesList(filteredDataset);
-                    recipeNo = Input.intInput("Enter -1 to go back or select the recipe no. to view it.");
-                    if (recipeNo == -1) {break;};
-                    View.fullRecipe(filteredDataset.row(recipeNo));
+                    searchSelector(3);
                     break;
                 case 4:
                     System.out.println("Going back to the main menu...");
