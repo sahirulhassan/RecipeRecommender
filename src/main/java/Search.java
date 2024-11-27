f9e77bb1030fdd3d2ede87a614a31bb62a0b7035
Sahir Ul Hassan <sahirulhasan2002@gmail.com>
2024-11-27 14:23:54 +0000

    Further enhanced the querying methods.

--- src/main/java/Search.java
+++ src/main/java/Search.java
@@ -1,5 +1,3 @@
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import tech.tablesaw.api.Table;
 
 import java.util.Arrays;
@@ -11,45 +9,22 @@ public class Search {
     public Search(Table dataset) {
         this.dataset = dataset;
     }
-//    Deprecated:
-//    public Table byIngredients(String[] list) throws JsonProcessingException {
-//        Column ingredientsCol = dataset.column("ingredients");
-//        ObjectMapper objectMapper = new ObjectMapper();
-//
-//        Table filteredDataset = dataset.emptyCopy();
-//        for (int i = 0; i < ingredientsCol.size(); i++) {
-//            String ingredientsStr = ingredientsCol.get(i).toString();
-//            String[] ingredientsArr = objectMapper.readValue(ingredientsStr, String[].class);
-//            if(ArrayComparison.isArraySubset(ingredientsArr, list)) {
-//                filteredDataset.append(dataset.row(i));
-//            }
-//        }
-//        return filteredDataset;
-//    }
 
     public Table byIngredients(String[] query) {
-        ObjectMapper mapper = new ObjectMapper();
         return dataset
                 .where(dataset
                         .textColumn("ingredients")
                         .eval(entry -> {
-            try {
-                String[] ingredientsEntry = mapper.readValue(entry, String[].class);
-                String stringifiedEntry = String.join(" ", ingredientsEntry)
-                        .toLowerCase();
-                return Arrays.stream(query)
-                        .allMatch(queryIngredient -> stringifiedEntry
-                                .contains(queryIngredient.toLowerCase()));
-            } catch (JsonProcessingException e) {
-                throw new RuntimeException(e);
-            }
-        } ));
+                            return Arrays.stream(query)
+                                    .allMatch(queryIngredient -> entry.toLowerCase()
+                                            .contains(queryIngredient.toLowerCase()));
+                        }));
     }
 
-    public Table byName(String name) {
+        public Table byName(String name) {
         return dataset
                 .where(dataset
                         .textColumn("name")
-                        .eval(entry -> entry.equalsIgnoreCase(name)));
+                        .eval(entry -> entry.toLowerCase().contains(name.toLowerCase())));
     }
 }
