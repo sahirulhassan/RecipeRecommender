8f6f7a07a7a63a36cd502d8e95ca7e1748552db0
Sahir Ul Hassan <sahirulhasan2002@gmail.com>
2024-11-28 18:53:46 +0000

    Cleaned up the menus looping logic.

--- src/main/java/Menu.java
+++ src/main/java/Menu.java
@@ -3,9 +3,6 @@ import tech.tablesaw.api.Table;
 import tech.tablesaw.io.csv.CsvReadOptions;
 
 public class Menu {
-
-    private static boolean masterLoop;
-    private static boolean loop;
     private static int selection;
     private static final Search search = new Search(datasetReader("src/main/resources/dataset.csv"));
 
@@ -16,17 +13,14 @@ public class Menu {
         return Table.read().csv(options);
     }
 
-
     public static void mainMenu() throws Exception {
-        masterLoop = true;
         System.out.println(View.centerAlign("Recipe Recommender", 100));
-        while (masterLoop) {
+        while (true) {
             System.out.printf(
                     View.centerAlign("Main Menu", 100) + "\n\n" +
                             "1. Search Recipes\n2. Surprise me\n3. Exit\n"
             );
             selection = Input.intInput("Select from the menu:");
-            loop = true;
             switch (selection) {
                 case 1:
                     searchMenu();
@@ -37,32 +31,22 @@ public class Menu {
                     break;
                 case 3:
                     System.out.println(View.centerAlign("Goodbye! Exiting...", 100));
-                    masterLoop = false;
-                    break;
+                    return;
             }
         }
     }
 
     private static void searchSelector(int searchMethod) throws Exception {
-        Table filteredDataset;
-        switch (searchMethod) {
-            case 1:
-                filteredDataset = search.byIngredients();
-                break;
-            case 2:
-                filteredDataset = search.byName();
-                break;
-            case 3:
-                filteredDataset = search.byKeywords();
-                break;
-            default:
-                throw new Exception();
-        }
-        int recipeNo;
-        while (loop) {
+        Table filteredDataset = switch (searchMethod) {
+            case 1 -> search.byIngredients();
+            case 2 -> search.byName();
+            case 3 -> search.byKeywords();
+            default -> throw new Exception();
+        };
+        while (true) {
             System.out.println(View.centerAlign("Found Results", 100));
             View.recipesList(filteredDataset);
-            recipeNo = Input.intInput("Enter -1 to go back or select the recipe no. to view it.");
+            int recipeNo = Input.intInput("Enter -1 to go back or select the recipe no. to view it.");
             if (recipeNo == -1) {
                 break;
             }
@@ -74,20 +58,15 @@ public class Menu {
                     break;
                 case 2:
                     System.out.println("Going back to the Search Menu...");
-                    loop = false;
-                    break;
+                    return;
             }
         }
     }
 
     private static void searchMenu() throws Exception {
-        masterLoop = true;
-        while (masterLoop) {
+        while (true) {
             System.out.print("1. Search by Ingredients\n2. Search by Recipe Names\n3. Search by Keywords\n4. Back\n");
             selection = Input.intInput("Select from the menu:");
-            Table filteredDataset;
-            int recipeNo;
-            loop = true;
             switch (selection) {
                 case 1:
                     searchSelector(1);
