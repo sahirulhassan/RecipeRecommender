15b7f9ba46f2cc025af3ec2034221d5e05c4fc5d
Sahir Ul Hassan <sahirulhasan2002@gmail.com>
2024-12-31 20:52:00 +0000

    Optimized imports.

--- src/main/java/recipeRecommender/Main.java
+++ src/main/java/recipeRecommender/Main.java
@@ -3,8 +3,8 @@ package recipeRecommender;
 import recipeRecommender.model.Reviews;
 import recipeRecommender.model.Search;
 import recipeRecommender.model.UserList;
-import recipeRecommender.view.Input;
 
+import static recipeRecommender.view.Input.stringInput;
 import static recipeRecommender.view.View.centerAlign;
 import static recipeRecommender.control.MainMenu.mainMenu;
 import static recipeRecommender.view.View.datasetReader;
@@ -18,12 +18,12 @@ public class Main {
     public static int width;
 
     public Main() {
-        username = Input.stringInput("Enter your username:");
+        username = stringInput("Enter your username:");
         System.out.println(centerAlign("\nWelcome to Recipe Recommender, " + username + "!"));
         System.out.println(centerAlign("Loading data...\n"));
 
         history = new UserList(10);
-        saved = new UserList(10);
+        saved = new UserList(20);
         reviews = new Reviews();
         width = 100;
         search = new Search(datasetReader("src/main/resources/dataset.csv"));
